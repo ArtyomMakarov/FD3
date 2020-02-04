4a674ce8644f3514603e4302fafb4801daf25c8c
Arsem Makarov <Makar6031994@gmail.com>
2020-02-05 00:44:43 +0300

    update Mobile task

--- Mobile/App.js
+++ Mobile/App.js
@@ -6,18 +6,17 @@ import ReactDOM from 'react-dom';
 import MobileCompany from './components/MobileCompany';
 
 let companyName='Velcom';
-let clientsArr=[ 
-  {id:101, fio:{last_name: "Иванов", name: "Иван", middle_name: "Иванович"}, balance:200, status: "active"},
-  {id:105, fio:{last_name: "Сидоров", name: "Сергей", middle_name: "Сергеевич"}, balance:250, status: "active"},
-  {id:110, fio:{last_name: "Петров", name: "Пётр", middle_name: "Петрович"}, balance:180, status: "active"},
-  {id:120, fio:{last_name: "Григорьев", name: "Григорий", middle_name: "Григорьевич"}, balance:-220, status: "blocked"},
+let clientsArr=[
+    {id:101, fio:{last_name: "Иванов", name: "Иван", middle_name: "Иванович"}, balance:200, status: "active"},
+    {id:105, fio:{last_name: "Сидоров", name: "Сергей", middle_name: "Сергеевич"}, balance:250, status: "active"},
+    {id:110, fio:{last_name: "Петров", name: "Пётр", middle_name: "Петрович"}, balance:180, status: "active"},
+    {id:120, fio:{last_name: "Григорьев", name: "Григорий", middle_name: "Григорьевич"}, balance:-220, status: "blocked"},
 ];
 
 ReactDOM.render(
-  <MobileCompany 
-    name={companyName}
-    clients={clientsArr}
-  />
-  , document.getElementById('container') 
+    <MobileCompany
+        name={companyName}
+        clients={clientsArr}
+    />
+    , document.getElementById('container')
 );
-
---
+++ Mobile/__tests__/ClientsButton-test.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import MobileClient from '../components/MobileClient';
+
+
+test('работа кнопки Редактировать MobileClient', ()=>{
+    let mode = 1;
+    let client = {id:101, fio:{last_name: "Иванов", name: "Иван", middle_name: "Иванович"}, balance:200, status: "active"};
+
+    const component = renderer.create(
+        <MobileClient mode={mode} info={client}/>
+    );
+    let componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+    const buttonElem = component.root.find( el => el.props.className =='inputEdit');
+    buttonElem.props.onClick();
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+    buttonElem.props.onClick();
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+});
+
+test('работа кнопки Удалить MobileClient', ()=>{
+    let mode = 1;
+    let client = {id:101, fio:{last_name: "Иванов", name: "Иван", middle_name: "Иванович"}, balance:200, status: "active"};
+    const component = renderer.create(
+        <MobileClient mode={mode} info={client}/>
+    );
+    let componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+    const buttonElem = component.root.find( el => el.props.className=='inputDelete');
+    buttonElem.props.onClick();
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+    buttonElem.props.onClick();
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+});
+
